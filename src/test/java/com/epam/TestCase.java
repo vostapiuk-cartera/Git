27b4b0075bbbd6a6f05f056f7cb241d6f4c3a780
Volodymyr Ostapiuk <volodymy.ostapiuk@cartera.com>
2022-07-26 19:28:54 +0300

    Change logic

--- src/test/java/com/epam/TestCase.java
+++ src/test/java/com/epam/TestCase.java
@@ -5,12 +5,21 @@ import org.testng.Assert;
 public class TestCase {
 
     @org.testng.annotations.Test
-    public void verify() {
+    public void verifyAdd() {
         int a = 1;
         int b = 0;
-        int c = b + 1;
+        int c = b + a;
         System.out.println("Verify test");
         Assert.assertEquals(c, a, "+ isn't working");
+    }
+
+    @org.testng.annotations.Test
+    public void verifyMultiply() {
+        int a = 1;
+        int b = 0;
+        int c = b * a;
+        System.out.println("Verify test");
+        Assert.assertEquals(c, b, "* isn't working");
     }
 
 }
