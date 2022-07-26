320d496ddded0855e2d63afefd62605bd7c2e3d8
Volodymyr Ostapiuk <volodymy.ostapiuk@cartera.com>
2022-07-26 18:42:58 +0300

    Add test

---
+++ src/test/java/com/epam/TestCase.java
@@ -0,0 +1,15 @@
+package com.epam;
+
+import org.testng.Assert;
+
+public class TestCase {
+
+    @org.testng.annotations.Test
+    public void verify() {
+        int a = 1;
+        int b = 0;
+        int c = b + 1;
+        Assert.assertEquals(c, a, "+ isn't working");
+    }
+
+}
